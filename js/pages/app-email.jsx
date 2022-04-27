5451274751e8aee97e2a4ab8aaa1ee942a728edc
Yael <julu3080@gmail.com>
2022-04-27 11:43:53 +0300

    add email page

---
+++ js/pages/app-email.jsx
@@ -0,0 +1,7 @@
+export class EmailApp extends React.Componnet{
+    render(){
+        return <section>
+            <h1>Hello email</h1>
+        </section>
+    }
+}
--- root-cmp.jsx
+++ root-cmp.jsx
@@ -1,5 +1,6 @@
 import { AppHeader } from './js/cmps/app-header.jsx'
 import { AppHome } from './js/pages/app-home.jsx'
+import {EmailApp} from './js/pages/app-email.jsx'
 
 const Router = ReactRouterDOM.HashRouter
 const { Route, Switch } = ReactRouterDOM
@@ -13,7 +14,7 @@ export function App() {
         <section className="app">
             <Switch>
                 {/* <Route path="/book" component={BookApp}></Route> */}
-                {/* <Route path="/about" component={About}></Route> */}
+                <Route path="/email" component={EmailApp}></Route>
                 <Route path="/" component={AppHome}></Route>
             </Switch>
         </section>
