b1ff45a92c775320ff5369cc139b69f5684a8022
MatanD <matandamary10@gmail.com>
2022-04-27 10:06:29 +0300

    Appsus project

---
+++ root-cmp.jsx
@@ -0,0 +1,10 @@
+
+const Router = ReactRouterDOM.HashRouter
+const { Route, Switch } = ReactRouterDOM
+
+export function App() {
+    return <section className="app">
+        <h1>Hello from App</h1>
+    </section>
+
+}
