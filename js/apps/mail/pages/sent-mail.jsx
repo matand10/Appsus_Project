e1b651bed18d68688577205339bec110e293a2bd
Yael <julu3080@gmail.com>
2022-05-02 01:50:03 +0300

    css sent tmail

--- js/apps/mail/pages/sent-mail.jsx
+++ js/apps/mail/pages/sent-mail.jsx
@@ -1,4 +1,4 @@
-import { UnReadCount } from '../../mail/cmps/unread-count.jsx'
+// import { UnReadCount } from '../../mail/cmps/unread-count.jsx'
 import { eventBusService } from '../../../services/event-bus-service.js'
 import { emailService } from '../../mail/services/email.service.js'
 import { SentMailsList } from '../cmps/sent-mail-list.jsx'
@@ -34,12 +34,12 @@ export class sentMails extends React.Component {
         const { sentMails } = this.state
         return <section className="email-app">
             <div className="email-board">
-                <SentMailsList sentMails={sentMails} removeMail={this.removeMail} />
                 <nav className="bar">
-                    <Link to="/email">Inbok</Link>
-                    <Link to="/sent">Sent</Link>
-                    <UnReadCount />
+                    <Link to="/email"><img src="assets/imgs/notes-imgs/inbox.svg" /> Inbok</Link>
+                    <Link to="/sent"><img src="assets/imgs/notes-imgs/send.svg" /> Sent</Link>
+                    {/* <UnReadCount /> */}
                 </nav>
+                <SentMailsList sentMails={sentMails} removeMail={this.removeMail} />
             </div>
         </section>
     }
